4a980d2a5baf56938fb97fd7a2d6397766c335d0
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-03-23 23:53:34 +0400

    Added base files for project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { Header } from "./Header";
+import "./App.css";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,5 @@
+export function Header() {
+    return(
+        <p>HEY</p>
+    )
+}
