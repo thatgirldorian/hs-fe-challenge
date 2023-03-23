58ee18bdb21ac4d5385aab851ad7c1160a5f169f
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-03-24 00:57:24 +0400

    Added header navigation + custom icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Header } from "./Header";
+
 import "./App.css";
 
 function App() {
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,19 @@
-import React from "react"
+import React from "react";
+import { MenuIcon } from "./assets/MenuIcon";
+import "./index.css";
 
 export function Header() {
-    return(
-        <nav>
-            
-        </nav>
-    )
+  return (
+    <nav className="bg-[#685EC5] mx-auto flex items-center justify-between px-8 py-4">
+      <div className="flex items-center">
+        <p className="text-white uppercase mr-2">Harbour.Space</p>
+        <p className="text-white text-[10px] uppercase mb-1">
+          /interaction design
+        </p>
+      </div>
+      <div className="md:block">
+        <MenuIcon className="menu-icon" />
+      </div>
+    </nav>
+  );
 }
