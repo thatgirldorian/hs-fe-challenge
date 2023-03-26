fdc6613daba29928acb7b335e0d487ea20770b4d
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-03-26 17:30:11 +0400

    Added scholarship details layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Header } from "./Header";
 import { ProgramInformation } from "./ProgramInformation";
+import { ScholarshipDetails } from "./ScholarshipDetails";
 
 function App() {
   return (
     <>
       <Header />
       <ProgramInformation />
+      <ScholarshipDetails />
     </>
   );
 }
---
+++ src/ScholarshipDetails.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export function ScholarshipDetails() {
+  return (
+    <>
+      <div className="my-[76px] grid grid-cols-3 mx-40 gap-4 pr-[40px]">
+        <div class="row-span-2 border-[1px] border-color[#DADADA] rounded-[4px] w-[320px] h-[524px]">
+          <p className="text-[16px] text-[color:var(--primary-color)]">
+            Scholarship value
+          </p>
+          <p className="text-[34px]">€31,300</p>
+          <hr></hr>
+          <div className="grid grid-cols-2">
+            <div className="">
+              <p className="text-[color:var(--primary-color)]">
+                Tuition covered
+              </p>
+              <p>€20,900</p>
+            </div>
+            <div className="">
+              <p className="text-[color:var(--primary-color)]">Remaining</p>
+              <p>€2,000</p>
+            </div>
+            <div className="">
+              <p className="text-[color:var(--primary-color)]">
+                Living Stipend
+              </p>
+              <p>€8,400 (€700/month)</p>
+            </div>
+          </div>
+        </div>
+        <div class="border-[1px] border-color[#DADADA] rounded-[4px] w-[320px] h-[274px]">
+          02
+        </div>
+        <div class="border-[1px] border-color[#DADADA] rounded-[4px] w-[320px] h-[274px]">
+          03
+        </div>
+        {/* <div>HI</div> */}
+        <div class="col-start-2 border-[1px] border-color[#DADADA] rounded-[4px] w-[680px] h-[178px]">
+          04
+        </div>
+      </div>
+    </>
+  );
+}
