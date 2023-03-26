592c76115ee0c78348d69dd9726c3cca2de5e597
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-03-26 19:05:42 +0400

    Added testimonial layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Header } from "./Header";
 import { ProgramInformation } from "./ProgramInformation";
 import { ScholarshipDetails } from "./ScholarshipDetails";
+import { Testimonials } from "./Testimonials";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <ProgramInformation />
       <ScholarshipDetails />
+      <Testimonials />
     </>
   );
 }
---
+++ src/Testimonials.jsx
@@ -0,0 +1,40 @@
+import React, { useRef, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+import "swiper/css";
+
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import { Navigation } from "swiper";
+
+export function Testimonials() {
+  return (
+    <div className="my-[279px] mx-40">
+      <Swiper
+        spaceBetween={50}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="mx-[280px] bg-red-400 border-[1px] border-color[#DADADA] rounded-[4px] w-[800px] h-[460px]">
+            Irene Pereya
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="mx-[280px] bg-yellow-200 border-[1px] border-color[#DADADA] rounded-[4px] w-[800px] h-[460px]">
+            Irene Pereya
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="mx-[280px] bg-blue-300 border-[1px] border-color[#DADADA] rounded-[4px] w-[800px] h-[460px]">
+            Irene Pereya
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
